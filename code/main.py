dcd8ed5980c4080588421961c7a140a3f51c683b
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-02-07 18:56:59 +0100

    init

---
+++ code/main.py
@@ -0,0 +1,30 @@
+from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Survivor')
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+    def run(self):
+        while self.running:
+            # dt 
+            dt = self.clock.tick() / 1000
+            
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    
+            # update
+            
+            # draw
+            pygame.display.update()
+            
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
