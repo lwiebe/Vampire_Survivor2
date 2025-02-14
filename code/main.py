cd21443f3c29457b11b7484de17f3c7e0e687dcf
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-02-14 13:26:15 +0100

    ground

--- code/main.py
+++ code/main.py
@@ -1,12 +1,20 @@
 from settings import *
+from player import Player
 
 class Game:
     def __init__(self):
+        # setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
+        
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+        
+        # sprites
+        self.player = Player((400,300), self.all_sprites)
         
     def run(self):
         while self.running:
@@ -19,8 +27,11 @@ class Game:
                     self.running = False
                     
             # update
+            self.all_sprites.update(dt)
             
             # draw
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
             
         pygame.quit()
