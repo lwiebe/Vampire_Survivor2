73a969188522ac394bec9abd0425ecbe4ca1f217
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-02-17 12:40:46 +0100

    camera

--- code/main.py
+++ code/main.py
@@ -1,5 +1,10 @@
 from settings import *
 from player import Player
+from sprites import *
+from pytmx.util_pygame import load_pygame
+from groups import AllSprites
+
+from random import randint
 
 class Game:
     def __init__(self):
@@ -11,10 +16,29 @@ class Game:
         self.running = True
         
         # groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pygame.sprite.Group()
         
-        # sprites
-        self.player = Player((400,300), self.all_sprites)
+        self.setup()
+        
+        # sprites 
+        
+
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE,y * TILE_SIZE), image, self.all_sprites)  
+            
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+        
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
         
     def run(self):
         while self.running:
@@ -31,7 +55,7 @@ class Game:
             
             # draw
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
             
         pygame.quit()
